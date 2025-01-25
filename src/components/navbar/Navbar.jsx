cd314cf225d272903d262c85baf251a418be8660
Chinweike99 <innocentakwolu1@gmail.com>
2025-01-25 09:26:22 +0100

    updated layout

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,33 +1,35 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import React from "react";
 
 const Navbar = () => {
-
-    const navBar = [
-        {id: 6, name: "Home", url: "/"},
-        {id: 1, name: "Potfolio", url: "/portfolio"},
-        {id: 2, name: "Blog", url: "/blog"},
-        {id: 3, name: "About", url: "/about"},
-        {id: 4, name: "Contact", url: "/contact"},
-        {id: 5, name: "Dashboard", url: "/dashboard"},
-    ] 
-
+  const navBar = [
+    { id: 6, name: "Home", url: "/" },
+    { id: 1, name: "Potfolio", url: "/portfolio" },
+    { id: 2, name: "Blog", url: "/blog" },
+    { id: 3, name: "About", url: "/about" },
+    { id: 4, name: "Contact", url: "/contact" },
+    { id: 5, name: "Dashboard", url: "/dashboard" },
+  ];
 
 
+  console.log("this is the navbar ..")
 
   return (
     <>
-        <div>
-            {navBar.map(clicks =>{
-                return (
-                    <div key={clicks.id}>
-                        <Link href={clicks.url}>{clicks.name}</Link>
-                    </div>
-                )
-            })}
-        </div>
+      <div>
+        {navBar.map((clicks) => {
+          return (
+            <div key={clicks.id}>
+              <Link href={clicks.url}>{clicks.name}</Link>
+            </div>
+          );
+        })}
+        <button onClick={(()=>{console.log("You are logged out")})}>Logout</button>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
