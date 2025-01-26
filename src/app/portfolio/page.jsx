1a0c5dcefe3ded09014dfb40609ff7cec45db1de
Chinweike99 <innocentakwolu1@gmail.com>
2025-01-26 22:31:47 +0100

    contact

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import styles from './page.module.css'
+import Link from 'next/link'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href='/portfolio/illustration' className={styles.item}>
+          <span></span>
+        </Link>
+      </div>
+    </div>
   )
 }
 
