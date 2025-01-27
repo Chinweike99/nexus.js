e2a004d365ba6af1201e2a69427d9418f6e90114
Chinweike99 <innocentakwolu1@gmail.com>
2025-01-27 06:50:09 +0100

    portfolio images

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -7,14 +7,14 @@ const Portfolio = () => {
     <div className={styles.container}>
       <h1 className={styles.selectTitle}>Choose a gallery</h1>
       <div className={styles.items}>
-        <Link href='/portfolio/illustrations' className={styles.item}>
-          <span>Illustrations</span>
+        <Link href='/portfolio/family' className={styles.item}>
+          <span>Family</span>
         </Link>
-        <Link href='/portfolio/websites' className={styles.item}>
-          <span>Websites</span>
+        <Link href='/portfolio/school' className={styles.item}>
+          <span>School</span>
         </Link>
-        <Link href='/portfolio/application' className={styles.item}>
-          <span>Applications</span>
+        <Link href='/portfolio/travel' className={styles.item}>
+          <span>Travel</span>
         </Link>
       </div>
     </div>
