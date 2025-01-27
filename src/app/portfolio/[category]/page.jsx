8f461ad60e34a1f588b210a4e5610072e7546f86
Chinweike99 <innocentakwolu1@gmail.com>
2025-01-27 07:38:24 +0100

    category

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,48 @@
+import Button from '@/components/Button/Button'
 import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
 
-const Category = () => {
+const Category = ({params}) => {
+  console.log(params)
   return (
-    <div>
-        Category page
+    <div className={styles.imgContainer}>
+        <h1 className={styles.category}>{params.category}</h1>
+        <div className={styles.item}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Testing</h1>
+            <p className={styles.description}>Description</p>
+
+            <Button text={"see more"} url={"#"}/>
+          </div>
+
+          <div className={styles.imgContainer}>
+            <Image src={'/images/family1.png'} 
+            fill={true}
+            alt='image'
+
+            className={styles.Button}
+            />
+          </div>
+          
+        </div>
+        <div className={styles.item}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>Testing</h1>
+            <p className={styles.description}>Description</p>
+
+            <Button text={"see more"} url={"#"}/>
+          </div>
+
+          <div className={styles.imgContainer}>
+            <Image src={'/images/family1.png'} 
+            fill={true}
+            alt='image'
+
+            className={styles.Button}
+            />
+          </div>
+        </div>
     </div>
   )
 }
