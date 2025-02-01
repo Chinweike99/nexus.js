eb2fca6b507b92b58c5ba7cdd981ddda51bd3be6
Chinweike99 <innocentakwolu1@gmail.com>
2025-02-01 01:17:17 +0100

    mongodb

---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,105 @@
+import travel from '../../../../public/images/travel.png'
+import nuclearFam from '../../../../public/images/nuclearFam.png'
+import students1 from '../../../../public/images/students1.png'
+import family1 from '../../../../public/images/family1.png'
+import dadKids from '../../../../public/images/dadKids.png'
+
+
+export const items = {
+    family: [
+        {
+            id: 1, 
+            title: "The first item on the list", 
+            desc: "This is the description",
+            image: travel
+        },
+        {
+            id: 1, 
+            title: "The second item on the list", 
+            desc: "This is the description",
+            image: nuclearFam
+        },
+        {
+            id: 1, 
+            title: "The third item on the list", 
+            desc: "This is the description",
+            image: students1
+        },
+        {
+            id: 1, 
+            title: "The fourth item on the list", 
+            desc: "This is the description",
+            image: family1
+        },
+        {
+            id: 1, 
+            title: "The fifth item on the list", 
+            desc: "This is the description",
+            image: dadKids
+        },
+    ],
+    school: [
+        {
+            id: 1, 
+            title: "The first item on the list", 
+            desc: "This is the description",
+            image: travel
+        },
+        {
+            id: 1, 
+            title: "The second item on the list", 
+            desc: "This is the description",
+            image: nuclearFam
+        },
+        {
+            id: 1, 
+            title: "The third item on the list", 
+            desc: "This is the description",
+            image: students1
+        },
+        {
+            id: 1, 
+            title: "The fourth item on the list", 
+            desc: "This is the description",
+            image: family1
+        },
+        {
+            id: 1, 
+            title: "The fifth item on the list", 
+            desc: "This is the description",
+            image: dadKids
+        },
+    ],
+    travel: [
+        {
+            id: 1, 
+            title: "The first item on the list", 
+            desc: "This is the description",
+            image: travel
+        },
+        {
+            id: 1, 
+            title: "The second item on the list", 
+            desc: "This is the description",
+            image: nuclearFam
+        },
+        {
+            id: 1, 
+            title: "The third item on the list", 
+            desc: "This is the description",
+            image: students1
+        },
+        {
+            id: 1, 
+            title: "The fourth item on the list", 
+            desc: "This is the description",
+            image: family1
+        },
+        {
+            id: 1, 
+            title: "The fifth item on the list", 
+            desc: "This is the description",
+            image: dadKids
+        },
+    ]
+}
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,50 +1,46 @@
-import Button from '@/components/Button/Button'
-import React from 'react'
-import styles from './page.module.css'
-import Image from 'next/image'
+import Button from '@/components/Button/Button';
+import React from 'react';
+import styles from './page.module.css';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+import { items } from './data';
 
-const Category = ({params}) => {
-  console.log(params)
+// Fetch data based on category
+const getData = (cat) => {
+  const data = items[cat];
+  if (!data) {
+    return notFound(); // Show 404 page if category doesn't exist
+  }
+  return data;
+};
+
+const Category = ({ params }) => {
+  const data = getData(params.category);
+
   return (
-    <div className={styles.imgContainer}>
-        <h1 className={styles.catTitle}>{params.category}</h1>
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>Testing</h1>
-            <p className={styles.description}>Description</p>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
 
-            <Button text={"see more"} url={"#"}/>
+      {data.map((info, index) => (
+        <div className={styles.item} key={index}>
+          <div className={styles.content}>
+            <h1 className={styles.title}>{info.title}</h1>
+            <p className={styles.description}>{info.desc}</p>
+            <Button text={'See More'} url={'#'} />
           </div>
 
           <div className={styles.imgContainer}>
-            <Image src={'/images/family1.png'} 
-            fill={true}
-            alt='image'
-
-            className={styles.Button}
-            />
-          </div>
-          
-        </div>
-        <div className={styles.item}>
-          <div className={styles.content}>
-            <h1 className={styles.title}>Testing</h1>
-            <p className={styles.description}>Description</p>
-
-            <Button text={"see more"} url={"#"}/>
-          </div>
-
-          <div className={styles.imgContainer}>
-            <Image src={'/images/family1.png'} 
-            fill={true}
-            alt='image'
-
-            className={styles.Button}
+            <Image
+              src={info.image}
+              fill={true}
+              alt={info.title}
+              className={styles.img}
             />
           </div>
         </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
